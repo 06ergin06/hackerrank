74bbcd5b4c1de1905797a6ee4e1bb9e8aa313861
06ergin06 <ibrahimh.ergin@gmail.com>
2023-08-15 22:29:43 +0300

    Hackerrank unused lines deleted

--- cakecandles/index.js
+++ cakecandles/index.js
@@ -1,34 +1,3 @@
-'use strict';
-
-const fs = require('fs');
-
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-
-let inputString = '';
-let currentLine = 0;
-
-process.stdin.on('data', function(inputStdin) {
-    inputString += inputStdin;
-});
-
-process.stdin.on('end', function() {
-    inputString = inputString.split('\n');
-
-    main();
-});
-
-function readLine() {
-    return inputString[currentLine++];
-}
-
-/*
- * Complete the 'birthdayCakeCandles' function below.
- *
- * The function is expected to return an INTEGER.
- * The function accepts INTEGER_ARRAY candles as parameter.
- */
-
 function birthdayCakeCandles(candles) {
     const maxAge = Math.max(...candles);
     let count = 0;
@@ -37,19 +6,6 @@ function birthdayCakeCandles(candles) {
             count ++;
         }
     }
-    return(count)
+    console.log(count)
 }
-
-function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
-
-    const candlesCount = parseInt(readLine().trim(), 10);
-
-    const candles = readLine().replace(/\s+$/g, '').split(' ').map(candlesTemp => parseInt(candlesTemp, 10));
-
-    const result = birthdayCakeCandles(candles);
-
-    ws.write(result + '\n');
-
-    ws.end();
-}
+birthdayCakeCandles([3,2,3,1])
--- compareTriplets/index.js
+++ compareTriplets/index.js
@@ -1,36 +1,3 @@
-"use strict";
-
-const fs = require("fs");
-
-process.stdin.resume();
-process.stdin.setEncoding("utf-8");
-
-let inputString = "";
-let currentLine = 0;
-
-process.stdin.on("data", function (inputStdin) {
-  inputString += inputStdin;
-});
-
-process.stdin.on("end", function () {
-  inputString = inputString.split("\n");
-
-  main();
-});
-
-function readLine() {
-  return inputString[currentLine++];
-}
-
-/*
- * Complete the 'compareTriplets' function below.
- *
- * The function is expected to return an INTEGER_ARRAY.
- * The function accepts following parameters:
- *  1. INTEGER_ARRAY a
- *  2. INTEGER_ARRAY b
- */
-
 function compareTriplets(a, b) {
   let alice = 0;
   let bob = 0;
@@ -41,26 +8,6 @@ function compareTriplets(a, b) {
       bob += 1;
     }
   }
-  return[alice,bob]
-
+  console.log([alice,bob])
 }
-
-function main() {
-  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
-
-  const a = readLine()
-    .replace(/\s+$/g, "")
-    .split(" ")
-    .map((aTemp) => parseInt(aTemp, 10));
-
-  const b = readLine()
-    .replace(/\s+$/g, "")
-    .split(" ")
-    .map((bTemp) => parseInt(bTemp, 10));
-
-  const result = compareTriplets(a, b);
-
-  ws.write(result.join(" ") + "\n");
-
-  ws.end();
-}
+compareTriplets([5,6,7],[3,6,10])
